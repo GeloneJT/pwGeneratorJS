0d5999e44ec9bcc7d706a060c26f4e651cd0cff1
Jelani Thomas <jelani13@icloud.com>
2021-02-05 07:25:56 -0500

    converted vars from strings to arrays

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var alphaBetLow ="abcdefghijklmnopqrstuvwxyz";
+var alphaBetLow =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 console.log(alphaBetLow);
 console.log(alphaBetLow.length);
-var alphaBetUP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var alphaBetUP = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 console.log(alphaBetUP);
 console.log(alphaBetUP.length);
-var numeric = "0123456789";
+var numeric = ['0','1','2','3','4','5','6','7','8','9'];
 console.log(numeric);
 console.log(numeric.length);
-var symbols = "!@#$%^&*()_+?<>?{}[]"
+var symbols = ['!','@','#','$','%','^','&','*','(',')','_','+','?','<','>','?','{','}','[',']']
 console.log(symbols);
 console.log(symbols.length);
 var alphaNumericSymb = alphaBetLow + alphaBetUP + numeric + symbols;
